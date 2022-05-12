88948f3260571097e8dea6facfe86d262d5c263a
SteffTraenkler <stefani.traenkler@mailbox.org>
2022-05-12 13:22:21 +0200

    added credentials in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,11 +8,27 @@ const { userRouter } = require("./routes/user-routes")
 const PORT = process.env.PORT || 9000
 const app = express()
 
-// app.use(cors({origin: [process.env.FRONTEND_URL], credentials: true}))
+app.use(cors({ origin: [process.env.FRONTEND_URL], credentials: true }))
+
+const oneDayinMs = 24 * 60 * 60 * 1000
+const isLocalHost = process.env.FRONTEND_URL === "http://localhost:3000"
+
+app.set('trust proxy', 1) //trust first proxy
+
+app.use(
+    cookieSession({
+        name: 'session',
+        secret: process.env.COOKIE_SESSION_SECRET,
+        httpOnly: true,
+        expires: new Date(Date.now() + oneDayinMs),
+        sameSite: isLocalHost ? 'lax' : 'none',
+        secure: isLocalHost ? false : true
+    })
+)
 
 app.use(morgan('dev')) // logger -> loggt Aktivitäten -> Middleware -> ((req,res),next)
 app.use(express.json()) //body parser für json
-app.use(express.static('assets'))
+app.use(express.static('assets')) //for picture birdie :0
 
 //Running test Route
 
