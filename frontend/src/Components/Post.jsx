7adca663bfeef668d28f934bf3edd9bf79921b4d
JostesMarcel <katzenfreund77@mein.gmx>
2022-05-17 13:18:39 +0200

    Neuer Post Bereich

--- frontend/src/Components/Post.jsx
+++ frontend/src/Components/Post.jsx
@@ -3,16 +3,20 @@ import { Link } from "react-router-dom";
 // import { RetweetIcon } from '../assets/img/RetweetIcon.png'
 // import { LikeIcon } from '../assets/img/LikeIcon.png'
 // import { ShareIcon } from '../assets/img/SahreIcon.png'
+import birdie from "../assets/img/Birdie.png";
 
 export default function Post(props) {
   return (
     <div className="post">
       <div className="userPic">
         <Link to={"/user/" + props.post.postedBy.username}>
-          <img
-            src={props.post.postedBy.profilePicture}
-            // alt={"Avatar of " + props.post.postedBy.username}
-          />
+          <div className="borderAvatar">
+            <img
+              // src={props.post.postedBy.profilePicture} // richtige Route!
+              src={birdie} // Nur zum stlyen
+              // alt={"Avatar of " + props.post.postedBy.username}
+            />
+          </div>
         </Link>
       </div>
       <div className="postBody">
@@ -27,10 +31,12 @@ export default function Post(props) {
           <p>{props.post.postText ? props.post.postText : ""}</p>
 
           {props.post.picture ? (
-            <img
-              src={props.post.picture}
-              alt={"Image Post by User " + props.post.postedBy.username}
-            />
+            <div className="box imgBox">
+              <img
+                src={props.post.picture}
+                alt={"Image Post by User " + props.post.postedBy.username}
+              />
+            </div>
           ) : (
             ""
           )}
