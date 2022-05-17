19ddcd1c903c42a4aeadbab7d54e4229331bc5c1
JostesMarcel <katzenfreund77@mein.gmx>
2022-05-17 16:05:32 +0200

    Neue post form & like button + zahl wird rot

--- frontend/src/Components/PostInteraction.jsx
+++ frontend/src/Components/PostInteraction.jsx
@@ -1,61 +1,65 @@
-import CommentIcon from '../assets/icons/tweeties/CommentIcon.png'
-import RetweetIcon from '../assets/icons/tweeties/RetweetIcon.png'
-import LikeIcon from '../assets/icons/tweeties/LikeIcon.png'
-import ShareIcon from '../assets/icons/tweeties/ShareIcon.png'
-import { apiBaseUrl } from '../api/api'
-import { useState } from 'react'
+import CommentIcon from "../assets/icons/tweeties/CommentIcon.png";
+import RetweetIcon from "../assets/icons/tweeties/RetweetIcon.png";
+import LikeIcon from "../assets/icons/tweeties/LikeIcon.png";
+import ShareIcon from "../assets/icons/tweeties/ShareIcon.png";
+import { apiBaseUrl } from "../api/api";
+import { useState } from "react";
+import "../styles/postInteraction.css";
 
 const LikeInteraction = (props) => {
+  const likePost = (event) => {
+    event.preventDefault();
 
-    const likePost = (event) => {
-        event.preventDefault()
+    fetch(apiBaseUrl + "/api/posts/like/" + props.post._id, {
+      method: "POST",
+      headers: {
+        token: "JWT " + props.token,
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        props.interactionChange
+          ? props.setInteractionChange(false)
+          : props.setInteractionChange(true);
+        console.log("likeToggle used...", data);
+      });
+  };
 
-        fetch(apiBaseUrl + "/api/posts/like/" + props.post._id, {
-            method: "POST",
-            headers: {
-                token: "JWT " + props.token
-            }
-        })
-            .then(resp => resp.json())
-            .then(data => {
-                props.interactionChange ? props.setInteractionChange(false) : props.setInteractionChange(true)
-                console.log("likeToggle used...", data)
-            })
-    }
-
-    return (
-        <div onClick={likePost} className={props.post.likedByUser ? "heart-liked" : "heart-dislike"}>
-            <img src={LikeIcon} alt="Link to like thi post / Tweet" />
-        </div>
-    )
-}
-
+  return (
+    <div
+      onClick={likePost}
+      className={props.post.likedByUser ? "heart-liked" : "heart-dislike"}
+    >
+      <img src={LikeIcon} alt="Link to like thi post / Tweet" />
+    </div>
+  );
+};
 
 const CommentInteraction = (props) => {
-    return (
-        <div>
-            <img src={CommentIcon} alt="Link to comment this tweet" />
-        </div>
-    )
-}
+  return (
+    <div>
+      <img src={CommentIcon} alt="Link to comment this tweet" />
+    </div>
+  );
+};
 
 const RetweetInteraction = (props) => {
-    return (
-        <div>
-            <img src={RetweetIcon} alt="Link to Retweet this Tweet" />
-        </div>
-    )
-}
+  return (
+    <div>
+      <img src={RetweetIcon} alt="Link to Retweet this Tweet" />
+    </div>
+  );
+};
 
 const ShareInteraction = (props) => {
-    <div>
-        <img src={ShareIcon} alt="Link to share this post / Tweet" />
-    </div>
-}
+  <div>
+    <img src={ShareIcon} alt="Link to share this post / Tweet" />
+  </div>;
+};
 
 export {
-    LikeInteraction,
-    CommentInteraction,
-    RetweetInteraction,
-    ShareInteraction
-}
+  LikeInteraction,
+  CommentInteraction,
+  RetweetInteraction,
+  ShareInteraction,
+};
