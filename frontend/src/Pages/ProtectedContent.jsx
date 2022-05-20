d3e4ba522924e54be6175cd742c4c9f60c07ad9a
JostesMarcel <katzenfreund77@mein.gmx>
2022-05-20 16:43:37 +0200

    Give page responisve desgin

--- frontend/src/Pages/ProtectedContent.jsx
+++ frontend/src/Pages/ProtectedContent.jsx
@@ -7,9 +7,11 @@ export default function ProtectedContent(props) {
 
     return (
         <div>
+
             <NavbarMain
                 token={props.token}
             />
+
             <Outlet />
             <NavbarBottom />
         </div>
